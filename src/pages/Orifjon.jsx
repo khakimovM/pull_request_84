18bca8c6fb45decea31df9b12e7eadf99ea16f96
Axmatov Orifjon <axmatovorifjon09@gmail.com>
2026-06-21 23:47:08 +0500

    orifjon page update

--- src/pages/Orifjon.jsx
+++ src/pages/Orifjon.jsx
@@ -4,6 +4,7 @@ const Orifjon = () => {
   return (
     <div>
       <h1>orifjon</h1>
+      <p>salom do'stlar bugun men mars typingda faxirli 3-o'rinda turibman menga omad tilasngizlar 2-oringaham chiqaman</p>
     </div>
   )
 }
