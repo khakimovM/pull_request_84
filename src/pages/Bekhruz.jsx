cdf1d2091e29e62f53691e123502c91b3e2cbfae
BehruzYavkachev <behruzyavkachev01@gmail.com>
2026-06-22 07:17:42 +0500

    Bekhruz added

---
+++ src/pages/Bekhruz.jsx
@@ -0,0 +1,11 @@
+ import React from 'react'
+ 
+ const Bekhruz = () => {
+   return (
+     <div>
+        <h1 className="text-2xl font-bold">Bekhruz</h1>
+     </div>
+   )
+ }
+ 
+ export default Bekhruz
